a4d0a4867e128ba55908f767e9181d5dba91866a
Robin Zigmond <robinzig@hotmail.com>
2019-10-20 23:37:35 +0100

    added custom instances for friendlier JSON and query values

--- src/SubsidiaryTypes.hs
+++ src/SubsidiaryTypes.hs
@@ -5,11 +5,12 @@
 module SubsidiaryTypes where
 
 import Data.Aeson
-import Data.Char (toLower)
-import Data.Text (pack)
+import Data.Text (pack, unpack)
 import Database.Persist.TH
 import GHC.Generics
 import Servant (FromHttpApiData(..))
+import Text.Casing (kebab, pascal)
+import Text.Read (readMaybe)
 import Web.HttpApiData (readTextData)
 
 
@@ -20,11 +21,16 @@ data CardType = Action | Treasure | Victory | Curse | Attack | Reaction | Durati
                 | Project
                 deriving (Eq, Show, Read, Generic)
 
-instance ToJSON CardType
-instance FromJSON CardType
+instance ToJSON CardType where
+    toJSON = String . pack . kebab . show
+
+instance FromJSON CardType where
+    parseJSON (String s) = case (readMaybe . pascal . unpack $ s :: Maybe CardType) of
+        Just c -> return c
+    parseJSON _ = fail "invalid type value given"
 
 instance FromHttpApiData CardType where
-    parseQueryParam = readTextData
+    parseQueryParam = readTextData . pack . pascal . unpack
 
 derivePersistField "CardType"
 
@@ -35,26 +41,28 @@ data Set = Base | BaseFirstEd | BaseSecondEd | Intrigue | IntrigueFirstEd
             | Adventures | Empires | Nocturne | Renaissance | Promo
             deriving (Eq, Show, Read, Generic)
 
-instance ToJSON Set
-instance FromJSON Set
+instance ToJSON Set where
+    toJSON = String . pack . kebab . show
+
+instance FromJSON Set where
+    parseJSON (String s) = case (readMaybe . pascal . unpack $ s :: Maybe Set) of
+        Just c -> return c
+    parseJSON _ = fail "invalid set value given"
 
 instance FromHttpApiData Set where
-    parseQueryParam = readTextData
+    parseQueryParam = readTextData . pack . pascal . unpack
 
 derivePersistField "Set"
-
 
 
 data CanDoIt = Always | Sometimes | Never deriving (Eq, Ord, Show, Read, Generic)
 
 instance ToJSON CanDoIt where
-    toJSON = String . pack . map toLower . show
+    toJSON = String . pack . kebab . show
 
 instance FromJSON CanDoIt where
-    parseJSON (String "always") = return Always
-    parseJSON (String "sometimes") = return Sometimes
-    parseJSON (String "never") = return Never
+    parseJSON (String s) = case (readMaybe . pascal . unpack $ s :: Maybe CanDoIt) of
+        Just c -> return c
     parseJSON _ = fail "invalid value - must be always, sometimes or never"
  
-
 derivePersistField "CanDoIt"
