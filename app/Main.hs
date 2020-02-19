de9ce28c86ade21fe8a48f30f97fee9b7b7d295e
Robin Zigmond <robinzig@hotmail.com>
2020-02-19 21:10:58 +0000

    preparing for Heroku deployment

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
-import Network.Wai.Handler.Warp
+import           Network.Wai.Handler.Warp
+import           System.Environment       (getEnv)
 
-import Complete (api)
-import Database (runLiveDB)
+import           Complete                 (api)
+import           Database                 (runLiveDB)
 
 main :: IO ()
-main = run 8000 $ api runLiveDB
+main = do
+  port <- read <$> getEnv "PORT"
+  run port $ api runLiveDB
